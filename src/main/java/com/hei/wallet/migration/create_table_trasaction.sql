f2e1f24e584f6d179a761e90addd025fa356a1ef
ManoaHEI <hei.manoa.4@gmail.com>
2023-12-04 16:39:12 +0300

    add currency column to transation table in migration script

--- src/main/java/com/hei/wallet/migration/create_table_trasaction.sql
+++ src/main/java/com/hei/wallet/migration/create_table_trasaction.sql
@@ -3,5 +3,7 @@ CREATE TABLE IF NOT EXISTS transaction (
     transaction transaction_type NOT NULL,
     amount FLOAT NOT NULL,
     id_receiver INT REFERENCES account(id_account),
-    id_sender INT REFERENCES account(id_account)
+    id_receiver_currency INT REFERENCES currency(id_currency),
+    id_sender INT REFERENCES account(id_account),
+    id_sender_currency INT REFERENCES currency(id_currency)
 );
